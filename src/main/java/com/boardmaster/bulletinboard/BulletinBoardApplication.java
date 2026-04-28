d28f639c107f41f21f390de1f04335ce7fb6f5d7
Ilya Belinsky <belinskyilya@gmail.com>
2026-04-28 23:39:49 +0300

    add build dependencies

--- src/main/java/com/boardmaster/bulletinboard/BulletinBoardApplication.java
+++ src/main/java/com/boardmaster/bulletinboard/BulletinBoardApplication.java
@@ -2,8 +2,10 @@ package com.boardmaster.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.boardmaster.bulletinboard.entity")
 public class BulletinBoardApplication {
 
 	public static void main(String[] args) {
